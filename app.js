dbc054079aa2017e205341da77ea4e5b33733072
jiamestoy <112114806+jiamestoy@users.noreply.github.com>
2022-12-02 00:44:02 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,77 @@
+if(navigator.serviceWorker){
+  navigator.serviceWorker.register('sw.js')
+}
+
+const btnSave = document.querySelector('#btn-save');
+const textArea = document.querySelector('#text-1');
+let btnEliminar = document.getElementsByClassName('btn-eliminar');
+let container = document.querySelector('.collection');
+let lista = [];
+
+document.addEventListener('DOMContentLoaded', function() {
+    let sideNav = document.querySelectorAll('.sidenav');
+    let instanciaSide = M.Sidenav.init(sideNav  , {});
+
+    let modal = document.querySelectorAll('.modal');
+    let instanciaModal = M.Modal.init(modal, {});
+
+    lista = leerNotas();
+    renderizarNotas(lista);
+});
+
+/* - FUNCION 1:  Obtiene el texto del textArea y guarda en el texto en el array - */
+btnSave.addEventListener('click', ()=>{
+
+  guardarNotas(lista);
+})
+
+/* -------- FUNCION 2: Recibe el array y lo guarda en el localStorage ------- */
+function guardarNotas(array){
+  
+  let dia = new Date().toLocaleDateString();
+  let contenido = textArea.value;
+  let nota = { nota: `${contenido}`, fecha: `${dia}`};
+  array.push(nota);
+  localStorage.lista = JSON.stringify(array);
+  textArea.value = '';
+  renderizarNotas(array);
+}
+
+/* --------- FUNCION 3: Lee los datos del localStorage y lo retorna --------- */
+function leerNotas(){
+
+  if (localStorage.lista) {
+    lista = JSON.parse(localStorage.lista);
+  }
+
+  return lista;
+}
+
+/* -------- FUNCION 4: Recibe el array y lo renderiza en el container ------- */
+function renderizarNotas(array){
+
+  container.innerHTML = '';
+
+  for (nota of array) {
+
+    let elemento = document.createElement("li");
+    elemento.className = "collection-item";
+    let div = document.createElement("div");
+    div.innerHTML = `${nota['nota']}`;
+    let aFecha = document.createElement("a");
+    aFecha.className = "secondary-content blue-text";
+    aFecha.innerHTML = `${nota['fecha']}`;
+    let aEliminar = document.createElement("a");
+    aEliminar.className = "secondary-content red-text btn-eliminar";
+    let iEliminar = document.createElement("i");
+    iEliminar.className = "material-icons";
+    iEliminar.innerHTML = `delete`;
+    aEliminar.appendChild(iEliminar);
+    div.appendChild(aEliminar);
+    div.appendChild(aFecha);
+    elemento.appendChild(div);
+    container.appendChild(elemento);
+
+  }
+
+}
---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'app-cache';
+
+self.addEventListener('install', evento => {
+
+    const respCache = caches.open(CACHE_NAME).then( cache => {
+
+        return cache.addAll([
+            '/',
+            'index.html',
+            'app.js',
+            'icons/icon-72x72.png',
+            'icons/icon-96x96.png',
+            'icons/icon-128x128.png',
+            'icons/icon-144x144.png',
+            'icons/icon-152x152.png',
+            'icons/icon-192x192.png',
+            'icons/icon-384x384.png',
+            'icons/icon-512x512.png',
+            'manifest.json',
+            'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css',
+            'https://fonts.googleapis.com/icon?family=Material+Icons'
+          ])
+          
+    })
+
+    evento.waitUntil(respCache);   
+})
+
+
+self.addEventListener('fetch', evento => {
+    const respuesta = fetch(  evento.request ).then( respNet => { 
+        return caches.open( CACHE_NAME ).then( cache => {
+            cache.put( evento.request, respNet.clone() );
+            return respNet;
+        })
+    }).catch( error => {
+        console.log(error);
+        return caches.match( evento.request  );
+    })
+    evento.respondWith(respuesta);
+})
